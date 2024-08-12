43df0a5fec08eece2099760e0fc53d488439bc20
InspiriaSCC <98588388+InspiriaSCC@users.noreply.github.com>
2024-08-12 13:57:49 +0200

    0.0.6

--- assets/js/binary.js
+++ assets/js/binary.js
@@ -1,4 +1,4 @@
-// meta={"simUrl":"https://trg-arcade.userpxt.io/v1.12.55/---simulator","cdnUrl":"https://pxt.azureedge.net","version":"0.0.5","target":"arcade","targetVersion":"1.12.55","repo":"inspiriascc/plattformtutorial"}
+// meta={"simUrl":"https://trg-arcade.userpxt.io/v1.12.55/---simulator","cdnUrl":"https://pxt.azureedge.net","version":"0.0.6","target":"arcade","targetVersion":"1.12.55","repo":"inspiriascc/plattformtutorial"}
 // total=1248161 new=5.85% cached=88.48% other=5.67%
 (function (ectx) {
 'use strict';
@@ -429,7 +429,7 @@ const pxsim_pxtrt = pxsim.pxtrt;
 const pxsim_numops = pxsim.numops;
 
 
-function _main___P73108(s) {
+function _main___P73185(s) {
 let r0 = s.r0, step = s.pc;
 s.pc = -1;
 
@@ -831,13 +831,13 @@ switch (step) {
     return leave(s, r0)
   default: oops()
 } } }
-_main___P73108.info = {"start":0,"length":0,"line":0,"column":0,"endLine":0,"endColumn":0,"fileName":"main.ts","functionName":"<main>","argumentNames":[]}
-_main___P73108.continuations = [  ]
+_main___P73185.info = {"start":0,"length":0,"line":0,"column":0,"endLine":0,"endColumn":0,"fileName":"main.ts","functionName":"<main>","argumentNames":[]}
+_main___P73185.continuations = [  ]
 
-function _main___P73108_mk(s) {
+function _main___P73185_mk(s) {
     checkStack(s.depth);
     return {
-        parent: s, fn: _main___P73108, depth: s.depth + 1,
+        parent: s, fn: _main___P73185, depth: s.depth + 1,
         pc: 0, retval: undefined, r0: undefined, overwrittenPC: false, lambdaArgs: null,
   tmp_0: undefined,
   tmp_1: undefined,
@@ -42328,5 +42328,5 @@ const sprites_BaseSpriteSayRenderer__C56840_VT = mkVTable({
 
 const breakpoints = setupDebugger(1, [])
 
-return _main___P73108
+return _main___P73185
 })
